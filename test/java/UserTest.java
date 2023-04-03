82f7d65ece1abb3cf7713fb887d81f0b4f0c0483
anuroopkanna <anuroopindia16@gmail.com>
2023-04-03 11:06:44 +0530

    added:uc11emailcalidation

--- test/java/UserTest.java
+++ test/java/UserTest.java
@@ -2,9 +2,19 @@ import com.BridgeLabzDay20.UserValidationTest;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
 
+@RunWith(Parameterized.class)
 public class UserTest {
+    String emailId;
+    public UserTest(String emailId){
+        super();
+        this.emailId=emailId;
+    }
     UserValidationTest user ;
     @Before
     public void initialize(){
@@ -17,48 +27,55 @@ public class UserTest {
     }
     @Test
     public void firstNameSadTest(){
-        boolean firstName = user.checkFirstName("sukanya");
+        boolean firstName = user.checkFirstName("anurop");
         Assert.assertFalse(firstName);
     }
     @Test
     public void checkLastNameHappyTest(){
-        boolean lastName = user.checkLastName("Vardam");
+        boolean lastName = user.checkLastName("Pulluru");
         Assert.assertTrue(lastName);
     }
     @Test
     public void checkLastNameSadTest(){
-        boolean lastName = user.checkLastName("aardam");
+        boolean lastName = user.checkLastName("ulluru");
         Assert.assertFalse(lastName);
     }
     @Test
     public void phoneNumberHappyTest(){
-        boolean phoneNumber = user.checkMobileNumber("919930516251");
+        boolean phoneNumber = user.checkMobileNumber("919848870082");
         Assert.assertTrue(phoneNumber);
     }
     @Test
     public void phoneNumberBadTest(){
-        boolean phoneNumber = user.checkMobileNumber("12221 9930516251");
+        boolean phoneNumber = user.checkMobileNumber("121 997488321");
         Assert.assertFalse(phoneNumber);
     }
     @Test
     public void passwordHappyTest(){
-        boolean password = user.checkPassword("Su@kanya123");
+        boolean password = user.checkPassword("lohith@123");
         Assert.assertTrue(password);
     }
     @Test
     public void passwordSadTest(){
-        boolean password = user.checkPassword("Sukanya123");
+        boolean password = user.checkPassword("Lohith123");
         Assert.assertFalse(password);
     }
     @Test
     public void emailHappyTest(){
-        boolean email = user.checkEmail("sukanya.44@gmai.com");
+        boolean email = user.checkEmail("anuroop.kannaiah@gmai.com");
         Assert.assertTrue(email);
     }
     @Test
     public void emailSadTest(){
-        boolean email = user.checkEmail("sukanya.@gmai.com");
+        boolean email = user.checkEmail("anuroop.@gmail.com");
         Assert.assertFalse(email);
     }
-
+    @Parameterized.Parameters
+    public static Collection input(){
+        return Arrays.asList( new String[] {"anuroop.kanna@gmai.com","anuroop123@gmail.com","abc@yahoo.com","abc@abc.com.au","abc@1.com","abc+100@gmail.com","abc.9@gmail.com.com","abc-100@abc.net","abc-100@yahoo.com"});
+    }
+    @Test
+    public void checkEmailWithMultipleInputs(){
+        Assert.assertEquals(true,user.checkEmail(emailId));
+    }
 }
